474231c1a9903660d7265a681a85ffb19a05c985
AdielSD <sephorazrpm@gmail.com>
2026-05-16 17:20:03 -0500

    week 01

---
+++ scripts/index.js
@@ -0,0 +1,16 @@
+/* Hamburguer bttn */
+const navbttn = document.querySelector('#ham-btn');
+const navBar = document.querySelector('#nav-bar');
+
+navbttn.addEventListener('click', () => {
+    navbttn.classList.toggle('show');
+    navBar.classList.toggle('show');
+});
+
+/* DATE CODE */
+
+const year = document.querySelector("#year");
+year.textContent = new Date().getFullYear();
+
+document.querySelector("#lastModified").textContent =
+"Last Modified: " + document.lastModified;
